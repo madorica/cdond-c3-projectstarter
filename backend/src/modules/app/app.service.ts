09fe6ef506d0b1e9b1bd94a82e52dd34f256e116
madorica <60213699+madorica@users.noreply.github.com>
2021-02-25 02:01:46 +0200

    Update app.service.ts

--- backend/src/modules/app/app.service.ts
+++ backend/src/modules/app/app.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
- import { JwtPayload } from '../auth/jwt-payload.interface';
+ //import { JwtPayload } from '../auth/jwt-payload.interface';
 
 @Injectable()
 export class AppService {
-   root(user: JwtPayload): string {
+   //root(user: JwtPayload): string {
   root(): string {
     return `Hello`;
   }
